cba8256b9f683677305981863a42574d9a8117ff
Bilol <acutmaxb@gmail.com>
2024-03-17 17:04:57 +0200

    Created and connected to sqlite d

--- Models/SrsContext.cs
+++ Models/SrsContext.cs
@@ -36,8 +36,7 @@ public partial class SrsContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-LFBV2SH\\SQLEXPRESS;Initial Catalog=SRS;Integrated Security=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        => optionsBuilder.UseSqlite("Data Source=SRS.db");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
