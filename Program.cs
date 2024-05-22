03603b9e35a98bf45b37e16289b17d7e63ad7536
jalol <jaloliddinqovlonov15@gmail.com>
2024-05-22 20:32:22 +0300

    program cs changed

--- Program.cs
+++ Program.cs
@@ -8,45 +8,56 @@ using Swashbuckle.AspNetCore.Filters;
 using System;
 using System.Text;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5174")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-	c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-	{
-		In = ParameterLocation.Header,
-		Name = "Authorization",
-		Type = SecuritySchemeType.ApiKey,
-	});
+    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+    });
 
-	c.OperationFilter<SecurityRequirementsOperationFilter>();
-	c.SwaggerDoc("v1", new OpenApiInfo { Title = "srs.webapi", Version = "1.0.0" });
-
+    c.OperationFilter<SecurityRequirementsOperationFilter>();
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "srs.webapi", Version = "1.0.0" });
 });
 
-
 builder.Services.AddAuthentication().AddJwtBearer(
-	options =>
-	{
-		options.TokenValidationParameters = new TokenValidationParameters
-		{
-			ValidateIssuerSigningKey = true,
-			ValidateLifetime = true,
-			ValidateAudience = false,
-			ValidateIssuer = false,
-			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
-				builder.Configuration.GetSection("JWT:Secret").Value!)),
-			ClockSkew = TimeSpan.Zero
-		};
-	}
+    options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                builder.Configuration.GetSection("JWT:Secret").Value!)),
+            ClockSkew = TimeSpan.Zero
+        };
+    }
 );
 
 builder.Services.AddControllersWithViews()
-	.AddNewtonsoftJson(options =>
-	options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+    .AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
 );
 
 builder.Services.AddScoped<IScheduleRepository, ScheduleRepository>();
@@ -58,9 +69,8 @@ builder.Services.AddScoped<ICourseRepository, CourseRepository>();
 builder.Services.AddScoped<IProfessorRepository, ProfessorRepository>();
 builder.Services.AddScoped<IStudentCourseRepository, StudentCourseRepository>();
 
-
 builder.Services.AddDbContext<SrsContext>(options =>
-		options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -71,6 +81,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
